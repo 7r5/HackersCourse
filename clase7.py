b853398e092aad3cbf83ca71eb31df2eae1b74da
Ricardo A <fma.7r5@gmail.com>
2025-02-24 22:18:47 -0600

    agregando un ejercicio

---
+++ clase7.py
@@ -0,0 +1,21 @@
+"""Aqui deben de pegar sus respuestas al ejercicio:
+Ejercicio: Determinar si un número es un múltiplo y está dentro de un rango
+Descripción: Escribe un programa en Python que solicite al usuario ingresar un número. El programa debe verificar si el número es múltiplo de 3 y si está dentro del rango de 10 a 50 (inclusive). Dependiendo de las condiciones, el programa debe imprimir diferentes mensajes.
+
+Instrucciones:
+
+Solicita al usuario que ingrese un número.
+
+Verifica si el número es múltiplo de 3.
+
+Verifica si el número está en el rango de 10 a 50 (inclusive).
+
+Imprime mensajes dependiendo de las verificaciones.
+"""
+# Respuesta Ricardo:
+
+# Respuesta Angel:
+
+# Respuesta Yanet:
+
+# Respuesta Elvia:
